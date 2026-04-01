3c71e4a809bccd448a4d1ec759401abca9e78824
Pyke Lee <lsh069283@gmail.com>
2026-04-01 14:23:38 +0900

    update

--- src/main/java/kr/pyke/command/IntegrationCommand.java
+++ src/main/java/kr/pyke/command/IntegrationCommand.java
@@ -13,7 +13,6 @@ import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Util;
 
-import java.awt.*;
 import java.net.URI;
 
 public class IntegrationCommand {
